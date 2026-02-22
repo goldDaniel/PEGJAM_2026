19122dde92a89d9161df3f1e27b388dad496c1bd
Nathaniel Plesniarski <nathanielples@outlook.com>
2026-02-22 11:30:40 -0600

    Credits

---
+++ Assets/Goblin/TMPLinkOpener.cs
@@ -0,0 +1,25 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TMPLinkOpener : MonoBehaviour, IPointerClickHandler
+{
+    private TextMeshProUGUI _text;
+
+    private void Awake()
+    {
+        _text = GetComponent<TextMeshProUGUI>();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        int linkIndex = TMP_TextUtilities.FindIntersectingLink(_text, eventData.position, eventData.pressEventCamera);
+        if (linkIndex != -1)
+        {
+            TMP_LinkInfo linkInfo = _text.textInfo.linkInfo[linkIndex];
+            string url = linkInfo.GetLinkID();
+
+            Application.OpenURL(url);
+        }
+    }
+}
